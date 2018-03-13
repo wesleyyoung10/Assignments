812355fc59829ced6006155432a31fb31811c96d
Wesley Young <wesleyyoung10@gmail.com>
2018-03-12 23:26:23 -0400

    Scoreboard

---
+++ Scoreboard/public/main.js
@@ -0,0 +1,18 @@
+const main = () => {
+  document.querySelector().textContent += '?'
+}
+
+function modify_qty(val) {
+  var qty = document.getElementById('qty').value;
+  var new_qty = parseInt(qty, 10) + val;
+
+  if (new_qty < 0) {
+    new_qty = 0;
+  }
+
+  document.getElementById('qty').value = new_qty;
+  return new_qty;
+}
+
+document.addEventListener('DOMContentLoaded', main)
+
